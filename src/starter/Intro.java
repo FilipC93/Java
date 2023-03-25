25c184b5383cd92285f327709bad19fbbfdf7fb1
Filip Cekani <66721991+FilipC93@users.noreply.github.com>
2023-03-25 22:16:41 +0200

    Second commits on Math

--- src/starter/Intro.java
+++ src/starter/Intro.java
@@ -3,13 +3,12 @@ package starter;
 public class Intro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String base = "Hello";
 		System.out.println(base);
 		char secondChar = base.charAt(1);
 		System.out.println(secondChar);
-		
-		for(int i = 0; i < base.length(); i++) {
+
+		for (int i = 0; i < base.length(); i++) {
 			System.out.println(base.charAt(i));
 		}
 	}
---
+++ src/starter/SolvingMath.java
@@ -0,0 +1,22 @@
+package starter;
+
+import java.util.Scanner;
+
+public class SolvingMath {
+
+	public static void main(String[] args) {
+		double x;
+		double y;
+		double z;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter side x: ");
+		x = scanner.nextDouble();
+		System.out.println("Enter side y: ");
+		y = scanner.nextDouble();
+
+		z = Math.sqrt((x * x) + (y * y));
+		System.out.println("The hypoteneuse is: " + z);
+		scanner.close();
+	}
+
+}
