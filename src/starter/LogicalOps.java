80288a75cd4a0d99e4d49496e360a68c2692f766
Filip Cekani <66721991+FilipC93@users.noreply.github.com>
2023-04-01 20:56:03 +0300

    Logical operators just as JS.

---
+++ src/starter/LogicalOps.java
@@ -0,0 +1,38 @@
+package starter;
+
+import java.util.Scanner;
+
+public class LogicalOps {
+
+	public static void main(String[] args) {
+		// Just like JS
+		// && = AND, || = OR ! = NOT
+
+		int temp = 25;
+
+		if (temp > 30) {
+			System.out.println("It's hot outside");
+		} else if (temp >= 20 && temp <= 30) {
+			System.out.println("It's between warm & hot outside");
+		} else {
+			System.out.println("It's cold outside!");
+		}
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("You're playing DOTA 2. Press Q or q to quit.");
+		String response = scanner.next();
+
+		if (response.equals("q") || response.equals("Q")) {
+			System.out.println("You quit!");
+		} else {
+			System.out.println("You're still playing!");
+		}
+		
+        //Opposite with ! operator.
+//		if (!response.equals("q") && !response.equals("Q")) {
+//			System.out.println("You're still playing!");
+//		} else {
+//			System.out.println("You quit!");
+//		}
+	}
+}
